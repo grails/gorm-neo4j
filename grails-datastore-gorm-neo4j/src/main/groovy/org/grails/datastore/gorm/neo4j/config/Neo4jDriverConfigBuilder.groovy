904cf96391e5ebbc07c5e839b13d8b61d82d9aa3
graemerocher <graeme.rocher@gmail.com>
2016-07-01 18:36:22 +0200

    Fix compilation error

--- grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/config/Neo4jDriverConfigBuilder.groovy
+++ grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/config/Neo4jDriverConfigBuilder.groovy
@@ -3,7 +3,7 @@ package org.grails.datastore.gorm.neo4j.config
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.grails.datastore.gorm.neo4j.Neo4jDatastore
-import org.grails.datastore.mapping.config.DatastoreConfigurationBuilder
+import org.grails.datastore.mapping.config.ConfigurationBuilder
 import org.neo4j.driver.v1.Config
 import org.springframework.core.env.PropertyResolver
 /**
@@ -14,7 +14,7 @@ import org.springframework.core.env.PropertyResolver
  */
 @CompileStatic
 @Slf4j
-class Neo4jDriverConfigBuilder extends DatastoreConfigurationBuilder<Config.ConfigBuilder, Config> {
+class Neo4jDriverConfigBuilder extends ConfigurationBuilder<Config.ConfigBuilder, Config> {
 
     Neo4jDriverConfigBuilder(PropertyResolver propertyResolver) {
         super(propertyResolver, Neo4jDatastore.SETTING_NEO4J_DRIVER_PROPERTIES, "with")
