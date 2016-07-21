890ce9718c04ef4707d55467e9bb62772d231314
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-21 14:27:11 +0200

    Release 6.0.0 M2

--- travis-build.sh
+++ travis-build.sh
@@ -4,12 +4,17 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 ./gradlew compileTestGroovy -x grails2-plugin:compileTestGroovy
-./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
-fi
-if [[ $EXIT_STATUS -eq 0 ]]; then 
-    ./gradlew gorm-neo4j-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+
+if [[ -n $TRAVIS_TAG ]]; then
+    ./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
+    if [[ $EXIT_STATUS -eq 0 ]]; then
+        ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+    fi
+    if [[ $EXIT_STATUS -eq 0 ]]; then
+        ./gradlew gorm-neo4j-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+    fi
+else
+    echo "Skipping tests to publish release"
 fi
 
 ./gradlew --stop
