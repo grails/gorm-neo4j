5b322d5b207d3ea91b232cbca03bd74f51221f12
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-21 15:08:29 +0200

    Back to snapshot

--- travis-build.sh
+++ travis-build.sh
@@ -6,6 +6,9 @@ EXIT_STATUS=0
 ./gradlew compileTestGroovy -x grails2-plugin:compileTestGroovy
 
 if [[ -n $TRAVIS_TAG ]]; then
+    echo "Skipping tests to publish release"
+
+else
     ./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
@@ -13,8 +16,6 @@ if [[ -n $TRAVIS_TAG ]]; then
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew gorm-neo4j-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
     fi
-else
-    echo "Skipping tests to publish release"
 fi
 
 ./gradlew --stop
