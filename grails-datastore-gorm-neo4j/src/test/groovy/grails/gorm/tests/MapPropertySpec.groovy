3c70a46a7ce128c2f704626f69ea2dd69b0e1bed
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-15 09:29:18 +0200

    add @NotYetImplemented test for maps

---
+++ grails-datastore-gorm-neo4j/src/test/groovy/grails/gorm/tests/MapPropertySpec.groovy
@@ -0,0 +1,32 @@
+package grails.gorm.tests
+
+import grails.gorm.annotation.Entity
+import grails.neo4j.Neo4jEntity
+import groovy.transform.NotYetImplemented
+
+/**
+ * Created by graemerocher on 09/09/2016.
+ */
+class MapPropertySpec extends GormDatastoreSpec {
+
+    @NotYetImplemented // neo4j doesn't support nested maps
+    void "Test persist map property"() {
+        when:"a object with a map property is persisted"
+
+        new Animal(name: 'Dog', attributes: [legs:4]).save(flush:true)
+        Animal a = Animal.first()
+        then:
+        a.name == "Dog"
+        a.attributes.size() == 1
+    }
+    @Override
+    List getDomainClasses() {
+        [Animal]
+    }
+}
+
+@Entity
+class Animal implements Neo4jEntity<Animal> {
+    String name
+    Map attributes
+}
