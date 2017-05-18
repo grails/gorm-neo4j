cb8994bdc9b7e5f3d3293958262d0b6a0f925699
jameskleeh <james.kleeh@gmail.com>
2017-05-18 15:47:08 -0400

    Expose the timestamp listener as a bean

--- grails-datastore-gorm-neo4j/src/main/groovy/grails/neo4j/bootstrap/Neo4jDataStoreSpringInitializer.groovy
+++ grails-datastore-gorm-neo4j/src/main/groovy/grails/neo4j/bootstrap/Neo4jDataStoreSpringInitializer.groovy
@@ -79,6 +79,7 @@ class Neo4jDataStoreSpringInitializer extends AbstractDatastoreInitializer {
                 validatorRegistry = new BeanFactoryValidatorRegistry((BeanFactory)beanDefinitionRegistry)
             }
             neo4jTransactionManager(neo4jDatastore:"getTransactionManager")
+            neo4jAutoTimestampEventListener(neo4jDatastore:"getAutoTimestampEventListener")
             neo4jDriver(neo4jDatastore:"getBoltDriver")
             neo4jPersistenceInterceptor(getPersistenceInterceptorClass(), ref("neo4jDatastore"))
             neo4jPersistenceContextInterceptorAggregator(PersistenceContextInterceptorAggregator)
