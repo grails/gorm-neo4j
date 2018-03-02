cc504bac558d99a765c573000fa6b56274657c96
jameskleeh <james.kleeh@gmail.com>
2018-03-02 14:17:02 -0500

    Return the session from the transaction if it exists

--- grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jSession.java
+++ grails-datastore-gorm-neo4j/src/main/groovy/org/grails/datastore/gorm/neo4j/Neo4jSession.java
@@ -213,7 +213,7 @@ public class Neo4jSession extends AbstractSession<Session> {
 
     @Override
     public Session getNativeInterface() {
-        return boltSession;
+        return hasTransaction() ? getTransaction().getBoltSession() : boltSession;
     }
 
     @Override
