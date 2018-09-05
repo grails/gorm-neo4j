7b383cea81db0674e912375ae05c564e764c847d
jameskleeh <james.kleeh@gmail.com>
2018-09-05 17:02:07 -0400

    Create our own tomcat container factory to prevent boot from creating a jetty one that is incompatible with the new version of jetty

--- examples/grails3-neo4j-hibernate/grails-app/init/functional/tests/Application.groovy
+++ examples/grails3-neo4j-hibernate/grails-app/init/functional/tests/Application.groovy
@@ -2,9 +2,18 @@ package functional.tests
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Bean
+    TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+        return new TomcatEmbeddedServletContainerFactory()
     }
 }
