66bcdb5ecb77e35e073ab4cecf74845a33f3ae94
sdelamo <sergio.delamo@softamo.com>
2018-12-06 11:36:36 +0100

    remove grails 2 references from travis

--- travis-build.sh
+++ travis-build.sh
@@ -3,16 +3,13 @@ EXIT_STATUS=0
 
 ./gradlew --stop
 
-./gradlew compileTestGroovy -x grails2-plugin:compileTestGroovy
+./gradlew compileTestGroovy
 
 if [[ -n $TRAVIS_TAG ]]; then
     echo "Skipping tests to publish release"
 
 else
-    ./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
-    if [[ $EXIT_STATUS -eq 0 ]]; then
-        ./gradlew grails2-plugin:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
-    fi
+    ./gradlew check --refresh-dependencies -no-daemon -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew gorm-neo4j-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
     fi
