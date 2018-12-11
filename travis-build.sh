ce2102591d30a7940441cb1f5c8921492412b863
sdelamo <sergio.delamo@softamo.com>
2018-12-11 19:16:31 +0100

    Add chrome and firefox tests
    Run tests with chromeHeadless

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,7 @@ if [[ -n $TRAVIS_TAG ]]; then
     echo "Skipping tests to publish release"
 
 else
-    ./gradlew check --refresh-dependencies -no-daemon -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
+    ./gradlew -Dgeb.env=chromeHeadless check --refresh-dependencies -no-daemon -x gorm-neo4j-spring-boot:test  || EXIT_STATUS=$?
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew gorm-neo4j-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
     fi
