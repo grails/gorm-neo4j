7d1302f4460d04a4d9c94769b45caee68af0fdba
Puneet Behl <behlp@objectcomputing.com>
2023-05-22 13:40:24 +0530

    Update OneToManyCreateSpec

--- grails-datastore-gorm-neo4j/src/test/groovy/grails/gorm/tests/cypher/domain/Owner.groovy
+++ grails-datastore-gorm-neo4j/src/test/groovy/grails/gorm/tests/cypher/domain/Owner.groovy
@@ -1,10 +1,11 @@
 package grails.gorm.tests.cypher.domain
 
 import grails.gorm.annotation.Entity
+import grails.neo4j.Neo4jEntity
 
 @Entity
 // tag::class[]
-class Owner {
+class Owner implements Neo4jEntity<Owner> {
     String name
     static hasMany = [pets:Pet]
 }
